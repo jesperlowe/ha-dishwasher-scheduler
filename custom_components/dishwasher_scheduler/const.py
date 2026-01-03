c18b43910477193df764f07fd067d6b19952db1e
jesperlowe <57229104+jesperlowe@users.noreply.github.com>
2026-01-03 20:30:16 +0100

    Remove binary icon asset

--- custom_components/dishwasher_scheduler/const.py
+++ custom_components/dishwasher_scheduler/const.py
@@ -6,10 +6,15 @@ CONF_START_BUTTON_ENTITY = "start_button_entity"
 CONF_READY_SUBSTRING = "ready_substring"
 CONF_WINDOW_START = "window_start"
 CONF_WINDOW_END = "window_end"
+CONF_PLANNING_MODE = "planning_mode"
+
+MODE_CHEAPEST_24H = "cheapest_24h"
+MODE_START_NOW = "start_now"
 
 DEFAULT_READY_SUBSTRING = "Ready"
-DEFAULT_WINDOW_START = 21
-DEFAULT_WINDOW_END = 5
+DEFAULT_WINDOW_START = 0
+DEFAULT_WINDOW_END = 0
+DEFAULT_PLANNING_MODE = MODE_CHEAPEST_24H
 
 PLATFORMS: list[str] = ["sensor", "switch"]
 
